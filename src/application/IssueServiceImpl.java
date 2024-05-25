f1e00d05adaf139b2adaab116153335aca6a0d38
zzznyeoneee <kjhn010226@gmail.com>
2024-05-25 23:32:10 +0900

    Update IssueServiceImpl.java

--- src/application/IssueServiceImpl.java
+++ src/application/IssueServiceImpl.java
@@ -54,6 +54,37 @@ public class IssueServiceImpl extends BaseServiceImpl<Issue> implements IssueSer
 		List<Issue> issues = new ArrayList<>();
 		
 		//DB의 모든 Issue를 불러옴
+		// 데이터베이스 연결
+        	DBService dbService = new DBService();
+        	Connection conn = dbService.getConnection();
+
+       		try {
+            		// SQL 쿼리
+            		String sql = "SELECT * FROM issues";
+
+            		// SQL 문 실행
+            		PreparedStatement statement = conn.prepareStatement(sql);
+            		ResultSet resultSet = statement.executeQuery();
+
+            		// 결과
+            		while (resultSet.next()) {
+                		String name = resultSet.getString("name");
+                		String description = resultSet.getString("description");
+               	 		String responsiblePL = resultSet.getString("responsiblePL");
+
+                		// Issue 객체 생성 후 리스트에 추가
+                		Issue issue = new Issue(name, description, responsiblePL);
+                		issues.add(issue);
+            		}
+
+            		resultSet.close();
+            		statement.close();
+        		} catch (SQLException e) {
+            			e.printStackTrace();
+        		} finally {
+           			// 연결 종료
+            			dbService.closeConnection();
+        		}
 		
 		return issues;
 	}
@@ -62,6 +93,30 @@ public class IssueServiceImpl extends BaseServiceImpl<Issue> implements IssueSer
 	protected void saveDataToDB() {
 
 		// dataList의 모든 Issue를 DB에 저장
+		// DB 연결
+		DBService dbService = new DBService();
+        	Connection conn = dbService.getConnection();
+
+        	try {
+           	 	// SQL문
+            		String sql = "INSERT INTO issues (name, description, responsiblePL) VALUES (?, ?, ?)";
+
+            		// SQL 문 실행 후 저장
+            		PreparedStatement statement = conn.prepareStatement(sql);
+            		for (Issue issue : dataList) {
+                		statement.setString(1, issue.getName());
+                		statement.setString(2, issue.getDescription());
+                		statement.setString(3, issue.getResponsiblePL());
+                		statement.executeUpdate();
+           		}
+
+            		statement.close();
+        		} catch (SQLException e) {
+            			e.printStackTrace();
+        		} finally {
+            		// 연결 종료
+            		dbService.closeConnection();
+        		}
 		
 	}
 
